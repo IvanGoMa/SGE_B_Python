66b025ae5ef4bb731b912c97adad3bb61963133d
IvanGoMa <99ivangm@gmail.com>
2024-11-07 11:49:14 +0100

    Exercici 1 fet

---
+++ Activitat_2_Exercici_1.py
@@ -0,0 +1,11 @@
+# 1. Comparar 2 números
+# Crear dues variables i comparar-les indicant quina és més gran, quina és més petita o si son iguals.
+num1 = 5
+num2 = 8
+if num1 > num2:
+    print(f'{num1}  és més gran que {num2}')
+elif num2 > num1:
+    print(f'{num2}  és més gran que {num1}')
+else:
+    print(f'{num2}  és igual que {num1}')
+
