91b93b0b48ee27e7aab7c2bfa313a157cdef6662
IvanGoMa <99ivangm@gmail.com>
2024-11-16 20:23:39 +0100

    Exercicis 1-6 fets

---
+++ Activitat_3_Exercici_2.py
@@ -0,0 +1,4 @@
+suma = 0
+for i in range (1,11):
+    suma += i
+print (f"la suma dels 10 primers nombres és: {suma}")
---
+++ Activitat_3_Exercici_6.py
@@ -0,0 +1,7 @@
+suma = 0
+i = 1
+while suma < 100:
+    print(f"{suma} + {i} = {suma + i}")
+    suma += i
+    i += 1
+    
