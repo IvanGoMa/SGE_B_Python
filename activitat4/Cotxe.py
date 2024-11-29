fb021b99598ef8d0572ac72d1e42e46ac241d5ff
IvanGoMa <99ivangm@gmail.com>
2024-11-29 10:59:46 +0100

    classes creades

---
+++ activitat4/Cotxe.py
@@ -0,0 +1,32 @@
+class Cotxe:
+    def __init__(self,marca,model,color,any,portes):
+        self.marca = marca
+        self.model = model
+        self.color = color
+        self.any = any
+        self.portes = portes
+
+    def getMarca(self):
+        return self.marca
+    def setMarca(self,newMarca):
+        self.marca= newMarca
+
+    def getModel(self):
+        return self.model
+    def setModel(self,newModel):
+        self.model= newModel
+
+    def getColor(self):
+        return self.color
+    def setColor(self,newColor):
+        self.color= newColor
+
+    def getAny(self):
+        return self.any
+    def setAny(self,newAny):
+        self.any= newAny
+
+    def getPortes(self):
+        return self.portes
+    def setPortes(self,newPortes):
+        self.portes= newPortes
